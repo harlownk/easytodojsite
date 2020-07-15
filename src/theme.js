216365db36d5eb1eae000cc6a3b66ae8e832dc15
Nick Harlow <harlownk@gmail.com>
2020-07-14 23:24:11 -0700

    Created and styled items for the page.
    Took another step closer to creating the page. I learned how to implement and style a little with Material Ui as well as their dark mode.

---
+++ src/theme.js
@@ -0,0 +1,10 @@
+import { createMuiTheme }  from '@material-ui/core/styles'
+
+const theme = createMuiTheme({
+    palette: {
+        primary: { main : '#00dcff' },
+        type: 'dark',
+    },
+});
+
+export default theme;
